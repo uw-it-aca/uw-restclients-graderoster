990e9ffe68d0a98d899e72ea05490888e14cf5af
Jim Laney <jlaney@uw.edu>
2026-02-11 12:43:00 -0800

    implements student_type

--- uw_sws_graderoster/models.py
+++ uw_sws_graderoster/models.py
@@ -80,6 +80,8 @@ class GradeRosterItem(models.Model):
                     gr_item.student_former_name = student_former_name
             elif classname == "student_number":
                 gr_item.student_number = el.text.strip()
+            elif classname == "student_type" and el.text is not None:
+                gr_item.student_type = el.text.strip()
             elif classname == "student_credits" and el.text is not None:
                 gr_item.student_credits = el.text.strip()
             elif "date_withdrawn" in classname and el.text is not None:
